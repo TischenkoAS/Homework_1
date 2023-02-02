c1cf2708edb0eae1a8ca0b19b38e9674208db1d9
«anastasiatischenko» <1510anastasia@mail.ru>
2023-02-02 13:21:12 +0300

    Добавили task6

---
+++ task6/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Clear();
+Console.Write("Введите число: ");
+double firstNumber = double.Parse(Console.ReadLine()!)!;
+
+if ((firstNumber % 2) != 0)
+{
+    Console.WriteLine("Введённое число нечётное.");
+}
+else
+{
+    Console.WriteLine("Введённое число чётное.");
+}
